1437e07ece680089b76ece72b28fe6c1d68fd6a2
MartinMurithi <wachiramartin442@gmail.com>
2023-07-25 01:43:40 +0300

    ..

--- client/src/components/forms/LogIn.jsx
+++ client/src/components/forms/LogIn.jsx
@@ -1,68 +1,66 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import Logo from "../logo/Logo";
 import "./Form.css";
 
 function LogIn() {
   const navigate = useNavigate();
   return (
-    <div className="signUp_signInSection">
-      <div className="signUpSection">
-        <div className="logoComponent">
-          <Logo />
+    <>
+      <div className="signUp_signInSection">
+        <div className="signInSection">
+          <div className="logoComponent">
+            <Logo />
+          </div>
+
+          <p className="signInText">Sign In to Kocima</p>
+
+          <form action="" className="signInForm">
+            <label htmlFor="userEmail" className="registerLabels">
+              Email <span className="mandatory">*</span>
+            </label>
+            <input
+              type="email"
+              placeholder="gordon@company.com"
+              className="registerInput"
+              id="userEmail"
+              required
+            />
+            <label htmlFor="password" className="registerLabels">
+              Password <span className="mandatory">*</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Min 8 characters"
+              minLength={8}
+              id="password"
+              className="registerInput"
+              required
+            />
+            <button className="signInBtn">Sign In</button>
+            <p className="signUpTextLink">Don't have an account? <NavLink
+              to={"/signUp"} className="signUpText">
+              Create Account</NavLink></p>
+          </form>
+          <p className="terms_conditions">
+            By signing up you agree to our <span className="terms">terms</span>{" "}
+            of service.
+          </p>
         </div>
 
-        <p className="createAcc">Sign In to Kocima</p>
+        {/* Bcg Image with sign up link */}
+        <div className="signUpLinkSection">
+          <p className="welcomeText">Hello There, Join Us</p>
+          <p className="loginText">
+            Enter your personal details and join the <br /> cooking community
+          </p>
+          <button className="signInLinkBtn" onClick={() => navigate("/signUp")}>
+            Sign Up
+          </button>
+        </div>
+      </div>
       
-      <form action="" className="signUpForm">
-        {/* <label htmlFor="userName" className="registerLabels">
-          Full name <span className="mandatory">*</span>
-        </label>
-        <input
-          type="text"
-          placeholder="Ramsey Gordon"
-          className="registerInput"
-          id="userName"
-          required
-        /> */}
-        <label htmlFor="userEmail" className="registerLabels">
-          Email <span className="mandatory">*</span>
-        </label>
-        <input
-          type="email"
-          placeholder="gordon@company.com"
-          className="registerInput"
-          id="userEmail"
-          required
-        />
-        <label htmlFor="password" className="registerLabels">
-          Password <span className="mandatory">*</span>
-        </label>
-        <input
-          type="password"
-          placeholder="Min 8 characters"
-          minLength={8}
-          id="password"
-          className="registerInput"
-          required
-        />
-        <button className="signUpBtn">Sign In</button>
-      </form>
-        <p className="terms_conditions">
-          By signing up you agree to our <span className="terms">terms</span> of
-          service.
-        </p>
-      </div>
-      <div className="loginLinkSection">
-        <p className="welcomeText">Hello There, Join Us</p>
-        <p className="loginText">
-          Enter your personal details and join the <br /> cooking community
-        </p>
-        <button className="signInLinkBtn" onClick={() => navigate("/signUp")}>
-          Sign Up
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
 
